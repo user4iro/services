e29d9a0ad4a7b5deac507c6dd1708b07052abe67
user4iro <ventas1agrosuministros@gmail.com>
2025-08-11 19:25:15 -0500

    Add files via upload

---
+++ shadcn-ui (4)/public/assets/images/payment-icons/payment-icons.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+
+// These are SVG components that can be used instead of image files
+
+export const PSEIcon = () => (
+  <svg viewBox="0 0 120 120" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-8 h-8">
+    <rect width="120" height="120" rx="60" fill="#0065B3" />
+    <path d="M85.5 40H34.5C32 40 30 42 30 44.5V75.5C30 78 32 80 34.5 80H85.5C88 80 90 78 90 75.5V44.5C90 42 88 40 85.5 40Z" fill="white" />
+    <path d="M38 61.5H49V65H38V61.5Z" fill="#0065B3" />
+    <path d="M38 54.5H49V58H38V54.5Z" fill="#0065B3" />
+    <path d="M72 54.5V65H76V61.5H83V58H76V54.5H72Z" fill="#0065B3" />
+    <path d="M52 54.5V65H63C66 65 68 63 68 60C68 57 66 54.5 63 54.5H52ZM56 58H61.5C62.5 58 63.5 58.5 63.5 60C63.5 61 62.5 61.5 61.5 61.5H56V58Z" fill="#0065B3" />
+  </svg>
+);
+
+export const NequiIcon = () => (
+  <svg viewBox="0 0 120 120" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-8 h-8">
+    <rect width="120" height="120" rx="60" fill="#E31A2F" />
+    <path d="M35 40H85C87.2091 40 89 41.7909 89 44V76C89 78.2091 87.2091 80 85 80H35C32.7909 80 31 78.2091 31 76V44C31 41.7909 32.7909 40 35 40Z" fill="white" />
+    <path d="M60 45C53.6348 45 48 49.4772 48 55C48 58.9 50.4 62.2 54 63.8V65H66V63.8C69.6 62.3 72 58.9 72 55C72 49.5 66.4 45 60 45ZM60 60C57.2 60 55 57.8 55 55C55 52.2 57.2 50 60 50C62.8 50 65 52.2 65 55C65 57.8 62.8 60 60 60Z" fill="#E31A2F" />
+    <path d="M44 70V75H52V70H44Z" fill="#E31A2F" />
+    <path d="M56 70V75H64V70H56Z" fill="#E31A2F" />
+    <path d="M68 70V75H76V70H68Z" fill="#E31A2F" />
+  </svg>
+);
+
+export const BancolombiaIcon = () => (
+  <svg viewBox="0 0 120 120" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-8 h-8">
+    <rect width="120" height="120" rx="60" fill="#FDB913" />
+    <path d="M30 40H90V80H30V40Z" fill="#EC1C24" />
+    <path d="M60 50L70 70H50L60 50Z" fill="white" />
+  </svg>
+);
+
+export const DaviplataIcon = () => (
+  <svg viewBox="0 0 120 120" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-8 h-8">
+    <rect width="120" height="120" rx="60" fill="#E2001A" />
+    <path d="M36 45C36 42.8 37.8 41 40 41H80C82.2 41 84 42.8 84 45V75C84 77.2 82.2 79 80 79H40C37.8 79 36 77.2 36 75V45Z" fill="white" />
+    <path d="M48 52H72V68C72 69.1 71.1 70 70 70H50C48.9 70 48 69.1 48 68V52Z" fill="#E2001A" />
+    <path d="M56 58H64V64H56V58Z" fill="white" />
+  </svg>
+);
+
+export const BanksIcon = () => (
+  <svg viewBox="0 0 120 120" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-8 h-8">
+    <rect width="120" height="120" rx="60" fill="#5271FF" />
+    <path d="M60 35L30 50V55H90V50L60 35Z" fill="#FFFFFF" />
+    <rect x="35" y="60" width="10" height="25" fill="#FFFFFF" />
+    <rect x="55" y="60" width="10" height="25" fill="#FFFFFF" />
+    <rect x="75" y="60" width="10" height="25" fill="#FFFFFF" />
+    <rect x="30" y="85" width="60" height="5" fill="#FFFFFF" />
+  </svg>
+);
+
+export const CashIcon = () => (
+  <svg viewBox="0 0 120 120" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-8 h-8">
+    <rect width="120" height="120" rx="60" fill="#00A650" />
+    <rect x="30" y="40" width="60" height="40" rx="2" fill="#FFFFFF" />
+    <circle cx="60" cy="60" r="10" fill="#00A650" />
+    <rect x="44" y="46" width="8" height="4" rx="1" fill="#00A650" />
+    <rect x="44" y="70" width="8" height="4" rx="1" fill="#00A650" />
+    <rect x="68" y="46" width="8" height="4" rx="1" fill="#00A650" />
+    <rect x="68" y="70" width="8" height="4" rx="1" fill="#00A650" />
+  </svg>
+);
+
+export const CreditCardIcon = () => (
+  <svg viewBox="0 0 120 120" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-8 h-8">
+    <rect width="120" height="120" rx="60" fill="#4B5AF4" />
+    <rect x="25" y="40" width="70" height="45" rx="5" fill="#FFFFFF" />
+    <rect x="25" y="50" width="70" height="10" fill="#4B5AF4" />
+    <rect x="35" y="70" width="30" height="5" rx="2" fill="#E0E0E0" />
+  </svg>
+);
+
+export const DebitCardIcon = () => (
+  <svg viewBox="0 0 120 120" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-8 h-8">
+    <rect width="120" height="120" rx="60" fill="#00AEEF" />
+    <rect x="25" y="40" width="70" height="45" rx="5" fill="#FFFFFF" />
+    <rect x="25" y="50" width="70" height="10" fill="#00AEEF" />
+    <rect x="35" y="70" width="30" height="5" rx="2" fill="#E0E0E0" />
+  </svg>
+);
